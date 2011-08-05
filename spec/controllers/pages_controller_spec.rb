68662c6cffb4f8b06f479f5f78886fe35c0822bb
Ayuba Audu <ayuba.audu@gmail.com>
2011-08-05 01:28:40 -0400

    Layout Update

--- spec/controllers/pages_controller_spec.rb
+++ spec/controllers/pages_controller_spec.rb
@@ -4,6 +4,10 @@ describe PagesController do
     
    render_views
    
+   before(:each) do
+      @base_title = "App"
+   end
+   
   describe "GET 'home'" do
     it "should be successful" do
       get 'home'
@@ -11,40 +15,53 @@ describe PagesController do
     end
     
    it "should have the right title" do
-    get 'home'
-    response.should have_selector("title", 
-                                                                    :content => "App | Home")
+      get 'home'
+      response.should have_selector("title", 
+                                                                    :content => @base_title + " | Home")
    end
    
    it "should have a non-blank body" do
-    get 'home'
-    response.body.should_not =~ /<body>\s*<\/body>/
-   end
+         get 'home'
+          response.body.should_not =~ /<body>\s*<\/body>/
+      end
   end
 
   describe "GET 'contact'" do
     it "should be successful" do
-      get 'contact'
-      response.should be_success
+         get 'contact'
+         response.should be_success
     end
     
-    it "should have the right title" do
-    get 'contact'
-    response.should have_selector("title", 
-                                                                    :content => "App | Contact")
-   end
+      it "should have the right title" do
+         get 'contact'
+         response.should have_selector("title", 
+                                                                    :content => @base_title + " | Contact")
+      end
   end
 
     describe "GET 'about'" do
         it "should be successful" do
-          get 'about'
-          response.should be_success
+            get 'about'
+            response.should be_success
         end
         
-        it "it should have the right title" do
-    get 'about'
-    response.should have_selector("title", 
-                                                                    :content => "App | About")
-   end
+         it "should have the right title" do
+            get 'about'
+            response.should have_selector("title", 
+                                                                    :content => @base_title + " | About")
+         end
+    end
+    
+    describe "GET 'help'" do
+      it "should be successful" do
+          get 'help'
+          response.should be_success
       end
+        
+      it "should have the right title" do
+         get 'help'
+         response.should have_selector("title", 
+                                                                    :content => @base_title + " | Help")
+      end
+   end
 end
